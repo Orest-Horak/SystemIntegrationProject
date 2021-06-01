e696c1219a646f01a16d65067df397b98f5d0c62
Orest-Horak <horak.orest@gmail.com>
2021-06-01 22:17:30 +0300

    update graphQL

---
+++ ScraperConsole/GraphNews/Models/NewsQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using DAL.Interfaces;
+using GraphQL.Types;
+using GraphQL;
+
+namespace GNews.Models
+{
+    public class NewsQuery : ObjectGraphType
+    {
+        public NewsQuery(INewsRepository newsRepository)
+        {
+            Field<ListGraphType<NewsType>>(
+                "news",
+                resolve: context => newsRepository.GetAllNewsAsync()
+            );
+
+            Field<ListGraphType<NewsType>>(
+                "newsById",
+                arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id"}),
+                 resolve: context => newsRepository.GetNewsById(context.GetArgument<string>("id"))
+            );
+        }
+    }
+}
